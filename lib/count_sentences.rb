974727df21c893362763389785d1a6b50eb69ad7
Jamie Masalinto <jmasalinto@gmail.com>
2020-11-28 17:48:41 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self ? self.end_with?(".") : false
   end
 
   def question?
-
+    self ? self.end_with?("?") : false
   end
 
   def exclamation?
-
+    self ? self.end_with?("!") : false
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
+  
 end
